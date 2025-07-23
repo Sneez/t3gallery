048f3a9c97d615ce5feeb8683fcffe244f16b666
Admin <Admin@MacBook.lan>
2025-07-23 15:33:07 -0700

    added auth with clerk

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export function TopNav() {
+  return(
+    <nav className="flex items-center justify-between w-full p-4 text-xl 
+      border-b font-semibold">
+      <div>Gallery</div>
+
+      <div>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  )
+}
