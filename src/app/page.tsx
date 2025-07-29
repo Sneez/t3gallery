61661c9f1bb15a9b916030a1579ec2d0367839a7
Admin <Admin@MacBook.lan>
2025-07-29 00:40:49 -0700

    started on modal parallel routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
 import Image from "next/image"
+import Link from "next/link";
 
 export const dynamic = "force-dynamic"
 
@@ -10,13 +11,15 @@ async function Images() {
       <div className="flex flex-wrap gap-4">
         {images.map((image) => (
           <div key={image.id} className="flex flex-col justify-center w-48">
-            <Image 
-              src={image.url} 
-              alt={image.name} 
-              style={{objectFit: "contain"}}
-              height={480}
-              width={480}
-            />
+            <Link href={`/img/${image.id}`}>
+              <Image 
+                src={image.url} 
+                alt={image.name} 
+                style={{objectFit: "contain"}}
+                height={480}
+                width={480}
+              />
+            </Link>
             <div>{image.name}</div>
           </div>
         ))}
