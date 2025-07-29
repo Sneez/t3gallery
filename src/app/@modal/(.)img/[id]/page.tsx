8dbc3e22bbee337fc9d66b55d8967a2e7d598f49
Admin <Admin@MacBook.lan>
2025-07-29 02:00:18 -0700

    parallel routes progress

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,9 +1,18 @@
+import { getImage } from "~/server/queries";
 
 export default async function PhotoModal({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const photoId = (await params).id;
-  return <div>{photoId}</div>;
+  const photoId = (await params).id
+  const idAsNumber = Number(photoId)
+  if (Number.isNaN(idAsNumber)) throw new Error ("Invalid photo id")
+
+  const image = await getImage(idAsNumber)
+  return (
+  <div>
+    <img src={image.url} className="w-96" />
+  </div>
+  ) 
 }
