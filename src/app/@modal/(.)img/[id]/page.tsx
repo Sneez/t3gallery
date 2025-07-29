4efe5d78956fcb17d5f0276a0a683b731a977869
Admin <Admin@MacBook.lan>
2025-07-29 02:19:55 -0700

    fixed modal and added fullscreen page on reload

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { getImage } from "~/server/queries";
+import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
 
 export default async function PhotoModal({
   params,
@@ -9,10 +10,9 @@ export default async function PhotoModal({
   const idAsNumber = Number(photoId)
   if (Number.isNaN(idAsNumber)) throw new Error ("Invalid photo id")
 
-  const image = await getImage(idAsNumber)
   return (
-  <div>
-    <img src={image.url} className="w-96" />
-  </div>
+  <Modal>
+    <FullPageImageView id={idAsNumber} />
+  </Modal>
   ) 
 }
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,9 +1,13 @@
+import FullPageImageView from "~/components/full-image-page";
 
 export default async function PhotoModal({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const photoId = (await params).id;
-  return <div>{photoId}</div>;
+  const photoId = (await params).id
+  const idAsNumber = Number(photoId)
+  if (Number.isNaN(idAsNumber)) throw new Error ("Invalid photo id")
+
+  return <FullPageImageView id={idAsNumber} /> 
 }
